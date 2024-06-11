fa2b4e521f50175a7fb7306bb3aa1215ad4a9f1b
martinbanc <140523328+martinbanc@users.noreply.github.com>
2024-06-11 12:46:23 +0100

    Initial commit

---
+++ investment_calculator.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var investmentAmount = 1000
+	var expectedReturn = 5.5
+	var years = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+	fmt.Println("Future value is", futureValue)
+
+}
